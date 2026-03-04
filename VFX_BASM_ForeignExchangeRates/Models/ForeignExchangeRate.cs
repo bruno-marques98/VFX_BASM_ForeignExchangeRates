fc934b22c559de96affc3516c879862880d6b245
bruno-marques98 <bruno98silva@hotmail.com>
2026-03-04 14:12:14 +0000

    Add project files.

---
+++ VFX_BASM_ForeignExchangeRates/Models/ForeignExchangeRate.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VFX_BASM_ForeignExchangeRates.Models
+{
+    public class ForeignExchangeRate
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string BaseCode { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(3)]
+        public string QuoteCurrency { get; set; } = string.Empty;
+
+        [Range(0, double.MaxValue)]
+        public decimal Bid { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Ask { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
